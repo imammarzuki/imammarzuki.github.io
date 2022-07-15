949cb8ce43a1a6c0ee2affcb880734b95c019697
imammarzuki <imammarzuki32@gmail.com>
2022-07-15 10:59:53 +0700

    Add files via upload

---
+++ kode/sakti1/hal55_2.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<title>Contoh List dengan HTML</title>
+</head>
+<body>
+<h2>Daftar Nomer</h2>
+<?php
+$i=1;
+while ($i<6){
+	echo "$i";
+	$i++;
+}
+?>
+</body>
+</html>
